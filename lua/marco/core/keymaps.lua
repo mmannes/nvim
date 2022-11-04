87f0c3687e2ef1a26c8aa365cb988adbbb9cf0ad
Marco Mannes <marco@mmannes.com>
2022-11-04 09:51:00 -0300

    initial commit

---
+++ lua/marco/core/keymaps.lua
@@ -0,0 +1,12 @@
+local keymap = vim.keymap
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") 
+
+-- telescope
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>") -- list available help tags
---
+++ lua/marco/plugins/indent-blankline.lua
@@ -0,0 +1,9 @@
+-- import lualine plugin safely
+local status, indent_blankline = pcall(require, "indent_blankline")
+if not status then
+	return
+end
+
+indent_blankline.setup({
+	show_current_context = true,
+})
